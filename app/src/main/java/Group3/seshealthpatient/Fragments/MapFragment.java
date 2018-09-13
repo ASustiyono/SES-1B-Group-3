33b9382f681365c6f3604d5e986d9388c6a4b495
jacobelali3 <jacobelali3@outlook.com>
2018-09-14 01:22:46 +1000

    googlemap-incomplete

--- app/src/main/java/Group3/seshealthpatient/Fragments/MapFragment.java
+++ app/src/main/java/Group3/seshealthpatient/Fragments/MapFragment.java
@@ -4,12 +4,14 @@ package Group3.seshealthpatient.Fragments;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment; //replaced
+//import android.support.design.widget.FloatingActionButton; //ui related
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 
 
 import com.google.android.gms.maps.GoogleMap;
@@ -20,7 +22,8 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import android.Manifest;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+
+
 import android.support.v4.content.ContextCompat;
 
 
@@ -48,15 +51,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback
         // Required empty public constructor
     }
 
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap)
-    {
-        Toast.makeText(this, "Maps is ready", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "Map is ready");
-        jacobsMap = googleMap;
-    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
@@ -65,98 +59,124 @@ public class MapFragment extends Fragment implements OnMapReadyCallback
         return inflater.inflate(R.layout.mapactivity, container, false);
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        Toast.makeText(getActivity(), "Maps is ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Map is ready");
+        jacobsMap = googleMap;
+    }
+
     private void initMap()
     {
         Log.d(TAG, "initializing map...");
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map); //ignore
 
         mapFragment.getMapAsync(MapFragment.this);
     }
+    /*private void userLocationFAB(){
+        FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.myLocationButton);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mapView.getMyLocation() != null) { // Check to ensure coordinates aren't null, probably a better way of doing this...
+                    mapView.setCenterCoordinate(new LatLngZoom(mapView.getMyLocation().getLatitude(), mapView.getMyLocation().getLongitude(), 20), true);
+                }
+            }
+        });*/
+
+
+
+    }
 
     private void getLocationPermission()
-    {
-        Log.d(TAG, "Retrieving location permissions");
-        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION};
-
-        if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
-                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
-            if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
-                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            Log.d(TAG, "Retrieving location permissions");
+            String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION};
+
+            if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(), //ignore
+                    FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
             {
-                LocationPermissionsGranted = true;
+                if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
+                        COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                {
+                    LocationPermissionsGranted = true;
+                }
+                else
+                   {
+                    requestPermissions( permissions,
+                            LOCATION_PERMISSION_CODE);
+                   }
             }
             else
                 {
                 requestPermissions( permissions,
                         LOCATION_PERMISSION_CODE);
                 }
-        }else{
-            requestPermissions( permissions,
-                    LOCATION_PERMISSION_CODE);
         }
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        Log.d(TAG, "onRequestPermissionsResult: called.");
-        LocationPermissionsGranted = false;
-
-        switch(requestCode)
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
         {
-            case LOCATION_PERMISSION_CODE:
+            Log.d(TAG, "onRequestPermissionsResult: called.");
+            LocationPermissionsGranted = false;
+
+            switch(requestCode)
+            {
+                case LOCATION_PERMISSION_CODE:
                 {
-                if (grantResults.length > 0)
-                {
-                    for (int i = 0; i < grantResults.length; i++)
+                    if (grantResults.length > 0)
                     {
-                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED)
+                        for (int i = 0; i < grantResults.length; i++) //ignore
                         {
-                            LocationPermissionsGranted = false;
-                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
-                            return;
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED)
+                            {
+                                LocationPermissionsGranted = false;
+                                Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                                return;
+                            }
                         }
+                        Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                        LocationPermissionsGranted = true;
+                        //if permissions are granted we initialize the map
+                        initMap();
                     }
-                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
-                    LocationPermissionsGranted = true;
-                    //if permissions are granted we initialize the map
-                    initMap();
                 }
             }
-        }
 
 
-    // integrate this method with the start of the application eg after a button has been clicked
-    // Debugging method- checks if google services is available/functional
-    public boolean isServicesOK()
-        {
-        Log.d(TAG, "isServicesOK: checking if your Google Services is up to date"); //debugging
+            // integrate this method with the start of the application eg after a button has been clicked
+            // Debugging method- checks if google services is available/functional
+            public boolean isServicesOK()
+            {
+                Log.d(TAG, "isServicesOK: checking if your Google Services is up to date"); //debugging
 
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapFragment.this); /*//log d = debug*/
+                int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapFragment.this); /*//log d = debug*/
 
-        if(available == ConnectionResult.SUCCESS)
-        {
-            /*//checks if everything is fine*/
-            Log.d(TAG, "isServicesOK: Google Play Services is functional");
-            return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
-        {
-            /* //If an error occurs, and it is resolvable*/
-            Log.d(TAG, "isServicesOK: A fixable error has occured!");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapFragment.this, available, ERROR_REQUEST);
-            dialog.show();  /*//This prompts google to provide a error message and solution when a fixable error has occurred*/
-        }
+                if(available == ConnectionResult.SUCCESS)
+                {
+                    /*//checks if everything is fine*/
+                    Log.d(TAG, "isServicesOK: Google Play Services is functional");
+                    return true;
+                }
+                else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+                {
+                    /* //If an error occurs, and it is resolvable*/
+                    Log.d(TAG, "isServicesOK: A fixable error has occured!");
+                    Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapFragment.this, available, ERROR_REQUEST);
+                    dialog.show();  /*//This prompts google to provide a error message and solution when a fixable error has occurred*/
+                }
 
-        else
+                else
 
-        {
-            Toast.makeText(this, "Google Play Services is dysfunctional, sorry!", Toast.LENGTH_SHORT).show(); /*//if it cant be fixed for whatever reason lmao*/
-        }
-        return false;
-    }
+                {
+                    Toast.makeText(getActivity(), "Google Play Services is dysfunctional, sorry!", Toast.LENGTH_SHORT).show(); /*//if it cant be fixed for whatever reason lmao*/
+                }
+                return false;
+            }
 
 
 
