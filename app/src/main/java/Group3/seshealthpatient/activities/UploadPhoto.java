200e88f1c5b0f3d6f6de1b9305d1da41234025b2
Krijan21 <krijancool.prajapati@gmail.com>
2018-10-02 11:11:32 +1000

    ImageFeature

---
+++ app/src/main/java/Group3/seshealthpatient/activities/UploadPhoto.java
@@ -0,0 +1,67 @@
+package group3.seshealthpatient.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import group3.seshealthpatient.R;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    private Button attachfile;
+
+    private StorageReference mStorage;
+
+    private static final int GALLERY_INTENT = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        attachfile = (Button) findViewById(R.id.attachfile_btn);
+
+        attachfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                intent.setType("image/*");
+
+                startActivityForResult(intent,GALLERY_INTENT );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+
+            Uri uri = data.getData();
+
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(UploadPhoto.this, "Upload Done", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
