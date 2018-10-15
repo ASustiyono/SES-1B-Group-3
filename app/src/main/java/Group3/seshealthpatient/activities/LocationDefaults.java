67e7b903d54af04cf5d2bd4ed88d3f18233f94b4
Andre <apagala@users.noreply.github.com>
2018-10-15 23:11:28 +1100

    Added more features

---
+++ app/src/main/java/Group3/seshealthpatient/activities/LocationDefaults.java
@@ -0,0 +1,10 @@
+package group3.seshealthpatient.activities;
+
+import com.google.android.gms.maps.model.LatLng;
+
+class LocationDefaults {
+    public static final LatLng DEFAULT_LOCATION = new LatLng(-33.86, 151.2);
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    public static final int DEFAULT_ZOOM = 15;
+    public static final int MEDIUM_ZOOM = 10;
+}
